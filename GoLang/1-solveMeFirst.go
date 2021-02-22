b0811177b0eba38476be8c030e0032d8a44a0bf1
fellipehfa <fellipehf@gmail.com>
2021-02-22 11:46:57 -0300

    New Challenge

---
+++ GoLang/1-solveMeFirst.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func solveMeFirst(a uint32, b uint32) uint32 {
+	soma := a + b
+	return soma
+}
+
+func main() {
+	var a, b, res uint32
+	fmt.Scanf("%v\n%v", &a, &b)
+	res = solveMeFirst(a, b)
+	fmt.Println(res)
+}
